9dbe2aeee1a13f444d8b9cef9ad3488f2a6b1494
Nethula Rankidu <wintergreen6631@proton.me>
2025-06-10 19:56:49 +0530

    Initial upload of the repository

---
+++ script.js
@@ -0,0 +1,52 @@
+function start() {
+    // Collecting all the editing parts of HTML (editing through the GET)
+    var birthdayTitle = document.getElementById("birthdayHeading");
+    var pfp = document.getElementById("birthdayPfp");
+    var nameWithTitle = document.getElementById("birthdayName");
+    var wish = document.getElementById("birthdayWish");
+    var sender = document.getElementById("birthdaySender");
+
+    // Finding the GET parameters from the URL and assigning them
+    const urlParams = new URLSearchParams(window.location.search);
+    const birthday_title = urlParams.get('title');
+    const profile_pic = urlParams.get('pfp');
+    const name_with_title = urlParams.get('name');
+    const birthday_wish = urlParams.get('wish');
+    const wish_sender = urlParams.get('sender');
+    console.log(birthday_title);
+    console.log(profile_pic);
+    console.log(name_with_title);
+    console.log(birthday_wish);
+    console.log(wish_sender);
+
+    if (birthday_title != null) {
+        birthdayTitle.innerHTML = birthday_title;
+    } else {
+        birthdayTitle.innerHTML = "Happy Birthday!";
+    }
+
+    if (profile_pic != null) {
+        pfp.src = profile_pic;
+    } else {
+        pfp.src = "assets/placeholder-pfp.png";
+    }
+
+    if (name_with_title != null) {
+        nameWithTitle.innerHTML = name_with_title;
+    } else {
+        nameWithTitle.innerHTML = "Dear User!";
+    }
+
+    if (birthday_wish != null) {
+        wish.innerHTML = birthday_wish;
+    } else {
+        wish.innerHTML = "Happy birthday to the best friend anyone could ask for! May your day be as amazing as you are.";
+    }
+
+    if (wish_sender != null) {
+        sender.innerHTML = wish_sender;
+    } else {
+        sender.innerHTML = "From: Anon";
+    }
+
+}
