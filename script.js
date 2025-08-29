0633195596589446b2f6ba0d9cfc0e2505db428c
Nethula Rankidu <wintergreen6631@proton.me>
2025-08-29 16:38:10 +0530

    Update script.js
    - checks if the GET is empty now (only null before)
    - added default strings to variables

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function start() {
-    // Collecting all the editing parts of HTML (editing through the GET)
+    // Text in HTML
     var birthdayTitle = document.getElementById("birthdayHeading");
     var pfp = document.getElementById("birthdayPfp");
     var nameWithTitle = document.getElementById("birthdayName");
@@ -7,7 +7,7 @@ function start() {
     var sender = document.getElementById("birthdaySender");
     var body = document.getElementById("body");
 
-    // Finding the GET parameters from the URL and assigning them
+    // Assigning GET values
     const urlParams = new URLSearchParams(window.location.search);
     const birthday_title = urlParams.get('title');
     const profile_pic = urlParams.get('pfp');
@@ -15,6 +15,8 @@ function start() {
     const birthday_wish = urlParams.get('wish');
     const wish_sender = urlParams.get('sender');
     const body_bg = urlParams.get('bg');
+
+    // printing in console
     console.log(birthday_title);
     console.log(profile_pic);
     console.log(name_with_title);
@@ -22,39 +24,72 @@ function start() {
     console.log(wish_sender);
     console.log(body_bg);
 
+    //default values to fallback
+    const bdTitleDef = "Happy Birthday!";
+    const pfpDef = "assets/placeholder-pfp.jpg";
+    const nameDef = "Dear User!";
+    const wishDef = "Happy birthday to the best friend anyone could ask for! May your day be as amazing as you are.";
+    const senderDef = "From: Anon";
+    const bodyBgDef = "background-image: url(assets/background.jpg)";
+
     if (birthday_title != null) {
-        birthdayTitle.innerHTML = birthday_title;
+        if (birthday_title.trim() === "") {
+            birthdayTitle.innerHTML = bdTitleDef;
+        }else{
+            birthdayTitle.innerHTML = birthday_title;
+        }
     } else {
-        birthdayTitle.innerHTML = "Happy Birthday!";
+        birthdayTitle.innerHTML = bdTitleDef;
     }
 
     if (profile_pic != null) {
-        pfp.src = profile_pic;
+        if (profile_pic.trim() === "") {
+            pfp.src = pfpDef;
+        }else{
+            pfp.src = profile_pic;
+        }
     } else {
-        pfp.src = "assets/placeholder-pfp.jpg";
+        pfp.src = pfpDef;
     }
 
     if (name_with_title != null) {
-        nameWithTitle.innerHTML = name_with_title;
+        if (name_with_title.trim() === "") {
+            nameWithTitle.innerHTML = nameDef;
+        }else{
+            nameWithTitle.innerHTML = name_with_title;
+        }
     } else {
-        nameWithTitle.innerHTML = "Dear User!";
+        nameWithTitle.innerHTML = nameDef;
     }
 
     if (birthday_wish != null) {
-        wish.innerHTML = birthday_wish;
+        if (birthday_wish.trim() === "") {
+            wish.innerHTML = wishDef;
+        }else{
+            wish.innerHTML = birthday_wish;
+        }
     } else {
-        wish.innerHTML = "Happy birthday to the best friend anyone could ask for! May your day be as amazing as you are.";
+        wish.innerHTML = wishDef;
     }
 
     if (wish_sender != null) {
+        if (wish_sender.trim() === "") {
+            sender.innerHTML = senderDef;
+        }else{
+            sender.innerHTML = wish_sender;
+        }
         sender.innerHTML = wish_sender;
     } else {
-        sender.innerHTML = "From: Anon";
+        sender.innerHTML = senderDef;
     }
 
     if (body_bg != null) {
-        body.style = "background-image: url("+ body_bg + ")";
+        if (wish_sender.trim() === "") {
+            body.style = bodyBgDef;
+        }else{
+            body.style = "background-image: url("+ body_bg + ")";
+        }
     } else {
-        body.style = "background-image: url(assets/background.jpg)";
+        body.style = bodyBgDef;
     }
 }
